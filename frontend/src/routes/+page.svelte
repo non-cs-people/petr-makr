0318757c42d88de49613eb123228f8a59ad16899
Ryan <leehahnwoong@gmail.com>
2023-07-15 14:11:42 -0700

    refactor: remove unnecessary code for project

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,24 +1,24 @@
 <script>
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	// import welcome from '$lib/images/svelte-welcome.webp';
+	// import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>What</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
 	<h1>
 		<span class="welcome">
-			<picture>
+			<!-- <picture>
 				<source srcset={welcome} type="image/webp" />
 				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
+			</picture> -->
 		</span>
 
-		to your new<br />SvelteKit app
+		testing<br />SvelteKit app
 	</h1>
 
 	<h2>

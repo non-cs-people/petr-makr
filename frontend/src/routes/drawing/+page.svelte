11cc98053c8f87104e3510b0e26e1c6f8561d24c
Ryan <leehahnwoong@gmail.com>
2023-07-23 15:23:35 -0700

    feat: add drawing component

---
+++ frontend/src/routes/drawing/+page.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { fabric } from 'fabric';
+
+  let canvas: fabric.Canvas | undefined;
+  let selectedColor = "#000000"; // Default color value
+  let brushSize = 5; // Default brush size
+
+
+  onMount(() => {
+    canvas = new fabric.Canvas('my-canvas', { isDrawingMode: true });
+    // Set the initial color and brush size for the canvas drawing brush
+    canvas.freeDrawingBrush.color = selectedColor;
+    canvas.freeDrawingBrush.width = brushSize;
+
+    let width = getWidth();
+
+    canvas.setWidth(width * canvas.getZoom());
+    canvas.setHeight(width * canvas.getZoom());
+  });
+
+  const getWidth = () => {
+    if(window.innerWidth < 600) return window.innerWidth;
+    return 600;
+  }
+  // Function to handle the colorChange event from the color picker
+  function handleColorChange(event: any) {
+    selectedColor = event.target.value;
+    // Update the drawing brush color in the canvas
+    canvas.freeDrawingBrush.color = selectedColor;
+  }
+
+  // Function to handle the brushSizeChange event from the brush size picker
+  function handleBrushSizeChange(event: any) {
+    brushSize = +event.target.value;
+    // Update the drawing brush size in the canvas
+    canvas.freeDrawingBrush.width = brushSize;
+  }
+
+  // Function to handle the drawing event on the canvas
+  function handleDrawing(event: any) {
+    if (canvas) {
+      const path = event.detail;
+      canvas.add(path);
+    }
+  }
+
+  // Function to clear the canvas
+  function clearCanvas() {
+    if (canvas) {
+      canvas.clear();
+    }
+  }
+</script>
+
+<style>
+  #my-canvas {
+    border: 1px solid #000;
+  }
+
+  /* Style the container for canvas and controls */
+  .canvas-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  /* Style the color picker input */
+  #color_picker {
+    margin-top: 10px;
+  }
+
+  /* Style the brush size picker input */
+  #brush_size_picker {
+    margin-top: 10px;
+  }
+</style>
+
+<h1>Drawing App</h1>
+<div class="canvas-container">
+  <canvas id="my-canvas" bind:this={canvas} width="800" height="400" on:drawing={handleDrawing}></canvas>
+  <input id="color_picker" type="color" bind:value={selectedColor} on:change={handleColorChange} />
+  <input id="brush_size_picker" type="range" min="1" max="20" step="1" bind:value={brushSize} on:input={handleBrushSizeChange} />
+  <button on:click={clearCanvas}>Clear Canvas</button>
+</div>
---
+++ frontend/src/routes/drawing/ColorPicker.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+  
+    let selectedColor = "#000000"; // Default color value
+  
+    // Create an event dispatcher to emit the color change event
+    const dispatch = createEventDispatcher();
+  
+    function handleColorChange(event: any) {
+      selectedColor = event.target.value;
+      // Emit the 'colorChange' event with the selected color as the payload
+      dispatch('colorChange', selectedColor);
+    }
+  </script>
+<input id="color_picker" type="color" bind:value={selectedColor} on:change={handleColorChange} />
+
+<!-- <style>
+</style> -->

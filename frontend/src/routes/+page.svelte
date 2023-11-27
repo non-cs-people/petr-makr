5a0acbefe4ed3e85e90638d255408097dab1144f
Jonathan Vigil <jvigil1738@gmail.com>
2023-11-27 14:42:16 -0800

    working accessories

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -141,13 +141,43 @@
         showOption('drawing');
       }
     }
-    let selectedOption = '';
-      function showOption(option: any) {
-        selectedOption = option;
-      }
 
-    function createAccessories() {
-      
+    let selectedOption = 'accessories';
+    function showOption(option: any) {
+      selectedOption = option;
+    }
+
+    const accMap = new Map<string, string>([
+      ["dva", "https://cdn.discordapp.com/attachments/1165802285448831086/1173444895822860450/Untitled_Artwork.png?ex=65766fb8&is=6563fab8&hm=fe7a4766a787e5e992493218c7db8ff3dc01e52823e6286a186c55e18ce09cee&"],
+      ["beanie", "https://cdn.discordapp.com/attachments/1165802285448831086/1173411645914087496/Untitled_Artwork.png?ex=657650c1&is=6563dbc1&hm=d5881bf9eece1cfecd263aa24cf479cb569a14e06a37d0b78ce01d90793225ce&"],
+      ["boba", "https://cdn.discordapp.com/attachments/1165802285448831086/1173434531475365888/0daf0032-28ce-4f5c-8ea6-c4065c76bc32.png?ex=65766611&is=6563f111&hm=8a762d1e357538483f88119b70616b6d354c0c30c1c6314ce718b4cad518ba99&"],
+      ["halloween basket", "https://cdn.discordapp.com/attachments/1165802285448831086/1173432918480257144/88c21aff-eb67-46f3-9af8-5b09599d3b61.png?ex=65766491&is=6563ef91&hm=327166b74392c5b6e72c0f8f634f69c05b2e82fd7941453f9ffa5b12a419de3d&"],
+      ["airpods", "https://cdn.discordapp.com/attachments/1165802285448831086/1173430838491365386/0e28b3a8-5ad5-45ec-bea1-be0e04635da2.png?ex=657662a1&is=6563eda1&hm=49ba67b7de2e7d7514f1c6db6ea08786879895538c833844cf55d6b6a6ee9617&"],
+      ["macbook", "https://cdn.discordapp.com/attachments/1165802285448831086/1173428576759074887/Untitled_Artwork.png?ex=65766085&is=6563eb85&hm=e0e7e7678a862642a2279983c7e766f90605eb7fe869958f707ce4cf23cced08&"],
+      ["stickered macbook", "https://cdn.discordapp.com/attachments/1165802285448831086/1173428266103754872/Untitled_Artwork.png?ex=6576603b&is=6563eb3b&hm=a875e3709d040fbfdb6efe03ba87eb3b8cb7204064f3837edaf182a3869bc36c&"],
+      ["kung fu robe",  "https://cdn.discordapp.com/attachments/1165802285448831086/1173427676384604170/yin_yang.png?ex=65765faf&is=6563eaaf&hm=64f44053624eb3cb32a4a9583e1a16f7f3047e312999774817ca0d06715c6ca6&"],
+      ["gucci belt", "https://cdn.discordapp.com/attachments/1165802285448831086/1173413422155702282/Untitled_Artwork.png?ex=65765268&is=6563dd68&hm=ef625bad70175cbea0f1d7da33114981338a4d608ab2c98b9b43ef08a6c59e92&"],
+      ["hearts", "https://cdn.discordapp.com/attachments/1165802285448831086/1173411577416912926/Untitled_Artwork.png?ex=657650b0&is=6563dbb0&hm=b6f132b0c3471ad4fa8131afac5066e3e3fe8449bea02f06f837807481c92f03&"],
+      ["jersey", "https://cdn.discordapp.com/attachments/1165802285448831086/1173411518671495308/Untitled_Artwork.png?ex=657650a2&is=6563dba2&hm=49f8387036e347e14bcb805c6f3429885faadb472e6506c7044c044eb04ea418&"],
+      ["gold chain", "https://cdn.discordapp.com/attachments/1165802285448831086/1173411352413487165/Untitled_Artwork.png?ex=6576507b&is=6563db7b&hm=fdcec3c8d271582d348b302266d2c8f7055d799b9f80cb3638e97e665c0473d2&"],
+      ["boots", "https://cdn.discordapp.com/attachments/1165802285448831086/1173411280888016996/Untitled_Artwork.png?ex=6576506a&is=6563db6a&hm=781695d7b02458f02c413c54fcc41760ca010e267e8ec687f15246b41c584fb1&"],
+      ["gojo shades", "https://cdn.discordapp.com/attachments/1165802285448831086/1173411221987393567/Untitled_Artwork.png?ex=6576505c&is=6563db5c&hm=9d2ab618dca7cb267d1730e26dc5913c7a5ba23d917193b2717029fbf7f5f1aa&"],
+      ["gojo hair", "https://cdn.discordapp.com/attachments/1165802285448831086/1173411111685599293/Untitled_Artwork.png?ex=65765041&is=6563db41&hm=06d9bac99bd2abd5f18a2bffadebcc1f197793ff0b1292e038b50fbf916b4abf&"],
+      ["apple watch", "https://cdn.discordapp.com/attachments/1165802285448831086/1165802341807702096/appleWatch.png?ex=65765189&is=6563dc89&hm=5b4bb55720ab23f1bfa58e28da9a79108daa4e597e491f3aa5873d83cdb84f19&"]
+
+    ]);
+    function showAccessory(option: string) {
+      let imgURL : string | undefined = accMap.get(option)
+      if (imgURL) {
+        fabric.Image.fromURL(imgURL, (img) => {
+          const canvasWidth = canvas.getWidth();
+          const canvasHeight = canvas.getHeight();
+          img.scaleToWidth(canvasWidth);
+          img.scaleToHeight(canvasHeight);
+          canvas.add(img);
+          canvas.renderAll();
+        });
+      }
     }
 
   </script>
@@ -166,7 +196,6 @@
     }
     #customize-bar{
       display: flex;
-      /* justify-content: space-around; */
       align-items: center;
       background-color: #29629F;
       color: white;
@@ -180,17 +209,17 @@
     }
     .nav-item{
       border-radius: 3px;
-      /* padding: 5px; */
       background-color: #f8d447;
       margin: 2px;
       align-items: center;
     }
-    .editbox{
+    .editbox {
       display: flex;
       flex-direction: column;
       justify-content: space-around;
       background-color: #FFE993;
       width: 70%;
+      height: 80%;
     }
 
     /* Style the container for canvas and controls */
@@ -232,6 +261,8 @@
         text-align: center;
     }
     .petr-options {
+      width: 80%;
+      height: 80%;
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -255,6 +286,29 @@
       display: flex;
       flex-direction: row;
     }
+    .outfit {
+      width: 100px;
+      height: auto;
+      border-radius: 50%;
+
+    }
+    .acc-content {
+      display: inline-block;
+      height: 40px;
+      widows: 100px;
+      padding: 5px;
+      margin: 5px;
+      overflow: scroll;
+    }
+    /* .button-grid button {
+      padding: 10px;
+      border: 1px solid #ccc;
+      cursor: pointer;
+    }
+
+    .button-grid button:active {
+      background-color: #007bff;
+    } */
   </style>
   
 <h1>Drawing App</h1>
@@ -265,7 +319,7 @@
       <div
         class="nav-item"
         on:click={() => showOption('accessories')}
-        on:keydown={createAccessories}
+        on:keydown={handleKeyPress}
         tabindex="0"
         role="button"
       >
@@ -304,7 +358,11 @@
       <!-- Conditionally render content based on the selected option -->
 
       {#if selectedOption === 'accessories'}
-        <div id="" class="petr-options">
+        <div class="button-grid">
+          {#each Array.from(accMap) as [option, url]}
+            <button class="acc-content" on:click={() => showAccessory(option)}> {option} </button>
+          {/each}
+
         </div>
       {/if}
 
